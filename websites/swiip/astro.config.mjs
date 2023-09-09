ac4d7934f708f5aa04e9f16e89c7749e8139c06e
Matthieu Lux <matthieu.lux@protonmail.com>
2023-09-09 21:26:17 +0200

    add vercel

--- websites/swiip/astro.config.mjs
+++ websites/swiip/astro.config.mjs
@@ -2,11 +2,13 @@ import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 import storyblok from "@storyblok/astro"
 import { loadEnv } from "vite"
+import vercel from "@astrojs/vercel/static"
 
 const env = loadEnv("", process.cwd(), "STORYBLOK")
 
 // https://astro.build/config
 export default defineConfig({
+	adapter: vercel({ analytics: true }),
 	integrations: [
 		tailwind(),
 		storyblok({
