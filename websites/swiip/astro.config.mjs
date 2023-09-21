77f2e445e8e3b2d58a9071246816f79df5bcd5c3
Matthieu Lux <matthieu.lux@protonmail.com>
2023-09-21 13:44:21 +0200

    more storyblok

--- websites/swiip/astro.config.mjs
+++ websites/swiip/astro.config.mjs
@@ -11,11 +11,16 @@ const env = loadEnv("", process.cwd(), "STORYBLOK")
 export default defineConfig({
 	adapter: vercel({ webAnalytics: true, speedInsights: true }),
 	integrations: [
-		tailwind(),
+		tailwind({
+			configFile: "./tailwind.config.ts",
+		}),
 		storyblok({
 			accessToken: env.STORYBLOK_TOKEN,
 			components: {
 				page: "storyblok/pages/Page",
+				title: "storyblok/bloks/Title",
+				link: "storyblok/bloks/Link",
+				header: "storyblok/bloks/Header",
 				hero: "storyblok/bloks/Hero",
 			},
 		}),
---
+++ websites/swiip/src/storyblok/types.d.ts
@@ -0,0 +1,121 @@
+import { StoryblokStory } from "storyblok-generate-ts"
+
+export interface HeaderStoryblok {
+	lead?: any[]
+	trail?: any[]
+	headline?: any[]
+	title?: any[]
+	_uid: string
+	component: "header"
+	[k: string]: any
+}
+
+export interface RichtextStoryblok {
+	type: string
+	content?: RichtextStoryblok[]
+	marks?: RichtextStoryblok[]
+	attrs?: any
+	text?: string
+	[k: string]: any
+}
+
+export interface HeroStoryblok {
+	title?: string
+	subtitle?: string
+	description?: string
+	content?: RichtextStoryblok
+	_uid: string
+	component: "hero"
+	[k: string]: any
+}
+
+export type MultilinkStoryblok =
+	| {
+			cached_url?: string
+			linktype?: string
+			[k: string]: any
+	  }
+	| {
+			id?: string
+			cached_url?: string
+			anchor?: string
+			linktype?: "story"
+			story?: {
+				name: string
+				created_at?: string
+				published_at?: string
+				id: number
+				uuid: string
+				content?: {
+					[k: string]: any
+				}
+				slug: string
+				full_slug: string
+				sort_by_date?: null | string
+				position?: number
+				tag_list?: string[]
+				is_startpage?: boolean
+				parent_id?: null | number
+				meta_data?: null | {
+					[k: string]: any
+				}
+				group_id?: string
+				first_published_at?: string
+				release_id?: null | number
+				lang?: string
+				path?: null | string
+				alternates?: any[]
+				default_full_slug?: null | string
+				translated_slugs?: null | any[]
+				[k: string]: any
+			}
+			[k: string]: any
+	  }
+	| {
+			url?: string
+			cached_url?: string
+			anchor?: string
+			linktype?: "asset" | "url"
+			[k: string]: any
+	  }
+	| {
+			email?: string
+			linktype?: "email"
+			[k: string]: any
+	  }
+
+export interface AssetStoryblok {
+	alt?: string
+	copyright?: string
+	id: number
+	filename: string
+	name: string
+	title?: string
+	focus?: string
+	[k: string]: any
+}
+
+export interface LinkStoryblok {
+	text?: string
+	link?: MultilinkStoryblok
+	icon?: AssetStoryblok
+	_uid: string
+	component: "link"
+	[k: string]: any
+}
+
+export interface PageStoryblok {
+	body?: any[]
+	_uid: string
+	component: "page"
+	uuid?: string
+	[k: string]: any
+}
+
+export interface TitleStoryblok {
+	text?: string
+	level?: string
+	_uid: string
+	component: "title"
+	[k: string]: any
+}
